827e49a4fc1353aa78f746752cd9c597c18b0673
Marcigdd <marcigdd@gmail.com>
2024-04-14 20:00:09 +0200

    fix: solve cors issue, use lambda proxy integration

--- lib/lambda/get-products.ts
+++ lib/lambda/get-products.ts
@@ -3,6 +3,11 @@ import { products } from "./mock-data/data";
 export async function main() {
   console.log("received request, sending back products");
   return {
-    products,
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(products),
   };
 }
