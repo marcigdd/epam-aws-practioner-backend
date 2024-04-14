1bd6be6fa57e47054cf442bacce62901fa27784a
Marcigdd <marcigdd@gmail.com>
2024-04-14 20:05:38 +0200

    feat: added simulated async call inside lambda

--- lib/lambda/get-product-by-id.ts
+++ lib/lambda/get-product-by-id.ts
@@ -1,13 +1,17 @@
-import { products } from "./mock-data/data";
+import { Product, products } from "./mock-data/data";
 
 export async function main(event: { id: string }) {
+  // Simulate an asynchronous operation with a Promise and setTimeout
+  const data: Product[] = await new Promise((resolve) =>
+    setTimeout(() => resolve(products), 1000)
+  );
   const id = event?.id;
   if (!id) {
     console.error("No id provided");
     throw new Error("No id provided");
   }
   console.log("received request, sending back product by id", { id });
-  const product = products.find((product) => product.id === id);
+  const product = data.find((product) => product.id === id);
   if (!product) {
     console.error("Product not found");
     throw new Error("Product not found");
--- lib/lambda/get-products.ts
+++ lib/lambda/get-products.ts
@@ -1,8 +1,17 @@
-import { products } from "./mock-data/data";
+import { Product, products } from "./mock-data/data";
 
 export async function main() {
   console.log("received request, sending back products");
+  // Simulate an asynchronous operation with a Promise and setTimeout
+  const data: Product[] = await new Promise((resolve) =>
+    setTimeout(() => resolve(products), 1000)
+  );
   return {
-    products,
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
+    body: JSON.stringify(data),
   };
 }
