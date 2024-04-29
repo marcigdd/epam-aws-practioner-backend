622f58cdc8f3ab8528fe6e032cfe30605558a5fa
Marcigdd <marcigdd@gmail.com>
2024-04-29 16:18:35 +0200

    fix: typeerror

--- lib/lambdas/get-products.ts
+++ lib/lambdas/get-products.ts
@@ -1,7 +1,7 @@
 import { productService } from "../services/product-service";
 import { ServerError } from "../util/custom-error";
 
-export async function main() {
+export async function main(event: any) {
   console.log(`Event: ${JSON.stringify(event)}`);
   try {
     const products = await productService.getProducts();
